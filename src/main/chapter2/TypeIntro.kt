e2ae59eade086b09b5291fb5fd376c1800bdc2e9
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-11 11:49:54 -0500

    redundant type information

--- src/main/chapter2/TypeIntro.kt
+++ src/main/chapter2/TypeIntro.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-  val playerName: String = "Estragon" // Making a variable immutable
+  val playerName: String = "Estragon" // Redundant type information -> String
   var experiencePoints :Int = 5
   experiencePoints += 5
   println(experiencePoints)
