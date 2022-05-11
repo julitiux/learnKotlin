d7ef28701da3745f1f75f4aa962fc8b67fe89606
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-10 23:44:58 -0500

    trying to change playerName’s value

--- src/main/chapter2/TypeIntro.kt
+++ src/main/chapter2/TypeIntro.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
   val playerName: String = "Estragon"
+  playerName = "Madrigal" // Error:(3, 5) Kotlin: Val cannot be reassigned
   var experiencePoints :Int = 5
   experiencePoints += 5
   println(experiencePoints)
