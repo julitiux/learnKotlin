7f6178cf0be487113b2401f00b05ac8a8fff8e8c
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-11 11:45:55 -0500

    changing playerName to be reassignable

--- src/main/chapter2/TypeIntro.kt
+++ src/main/chapter2/TypeIntro.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-  val playerName: String = "Estragon"
+  var playerName: String = "Estragon"
   var experiencePoints :Int = 5
   experiencePoints += 5
   println(experiencePoints)
