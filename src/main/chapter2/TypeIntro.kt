917954583b94c62a9f5b384c686c388c87564118
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-11 11:50:35 -0500

    removing explicit type specification

--- src/main/chapter2/TypeIntro.kt
+++ src/main/chapter2/TypeIntro.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-  val playerName: String = "Estragon" // Redundant type information -> String
+  val playerName = "Estragon" // Removing explicit type specification
   var experiencePoints :Int = 5
   experiencePoints += 5
   println(experiencePoints)
