ae011d7b00da1ae58c809275da7e0b2eeb9eacce
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-11 13:23:27 -0500

    Challenge: The Unicorn’s Horn

--- src/main/chapter2/TypeIntro.kt
+++ src/main/chapter2/TypeIntro.kt
@@ -9,6 +9,13 @@ fun main(args: Array<String>) {
   println(experiencePoints)
   println(playerName)
 
-  var hasSteed : Boolean
+  println ("Do you need to stable a steed?")
+  val hasSteed = false
+  if(hasSteed)
+    println("yes")
+  else
+    println("No, I have no steed. But I do have 50 gold pieces, and I would like a drink.")
+  println("Excellent!")
+  println("I have mead, wine, and LaCroix. What will you have?")
 
 }
