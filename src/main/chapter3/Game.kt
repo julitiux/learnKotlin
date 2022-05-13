f6d6a19e725553fa9fcc6d4f10af3250a1eab97c
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-13 18:34:04 -0500

    modifying healthPoints

--- src/main/chapter3/Game.kt
+++ src/main/chapter3/Game.kt
@@ -4,11 +4,11 @@
 fun main(args: Array<String>) {
 
   val name = "Madrigal"
-  var healthPoints = 100
+  var healthPoints = 90
 
   if (healthPoints == 100)
     println(name + " is in a excelente condition !")
   else
-    println(name + " is un a awful condition!")
+    println(name + " is an awful condition!")
 
 }
