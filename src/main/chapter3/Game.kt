df12db8a3315a3477cd7709fd29d2b18786373bb
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-13 18:32:42 -0500

    printing the player’s health condition

--- src/main/chapter3/Game.kt
+++ src/main/chapter3/Game.kt
@@ -1,6 +1,14 @@
 
 // Chapter 3
 
-fun main(args: List<String>) {
+fun main(args: Array<String>) {
+
+  val name = "Madrigal"
+  var healthPoints = 100
+
+  if (healthPoints == 100)
+    println(name + " is in a excelente condition !")
+  else
+    println(name + " is un a awful condition!")
 
 }
