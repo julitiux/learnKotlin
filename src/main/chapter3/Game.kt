05cf7e0e739b479ce67a5037215f9410910710b6
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-14 22:36:50 -0500

    refactoring health Status with when

--- src/main/chapter3/Game.kt
+++ src/main/chapter3/Game.kt
@@ -13,13 +13,16 @@ fun main(args: Array<String>) {
 
   println(auraColor)
 
-  val healthStatus = if (healthPoints == 100) "is in a excelente condition !"
-  else if (healthPoints in 90..99) "has a few scratches."
-  else if (healthPoints in 75..89)
-    if (isBlessed) "has some  minor wounds but is healing quite quickly!"
-    else "has some minor wounds."
-  else if (healthPoints in 15..74) "looks pretty hurt."
-  else "is an awful condition!"
+  val healthStatus = when (healthPoints) {
+    100 -> "is in a excelente condition !"
+    in 90..99 -> "has a few scratches."
+    in 75..89 -> if (isBlessed)
+      "has some  minor wounds but is healing quite quickly!"
+    else
+      "has some minor wounds."
+    in 15..74 -> "looks pretty hurt."
+    else -> "is an awful condition!"
+  }
 
   // Player Status
   println(name + " " + healthStatus)
