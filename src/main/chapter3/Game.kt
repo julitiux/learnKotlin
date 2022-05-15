e0fb700c3e74b1408d5e6ac09f0fa3105dc0e307
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-14 21:05:13 -0500

    checking for more player conditions

--- src/main/chapter3/Game.kt
+++ src/main/chapter3/Game.kt
@@ -1,4 +1,3 @@
-
 // Chapter 3
 
 fun main(args: Array<String>) {
@@ -8,6 +7,12 @@ fun main(args: Array<String>) {
 
   if (healthPoints == 100)
     println(name + " is in a excelente condition !")
+  else if (healthPoints >= 90)
+    println(name + " has a few scratches.")
+  else if (healthPoints >= 75)
+    println(name + " has some minor wounds.")
+  else if (healthPoints >= 15)
+    println(name + " looks pretty hurt.")
   else
     println(name + " is an awful condition!")
 
