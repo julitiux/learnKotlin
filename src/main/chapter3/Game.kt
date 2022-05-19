6085bb8f2ed0a4b363e67eb4f0878d96359debba
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-18 23:37:15 -0500

    trying Out Some Ranges

--- src/main/chapter3/Game.kt
+++ src/main/chapter3/Game.kt
@@ -31,4 +31,14 @@ fun main(args: Array<String>) {
   )
   println("$name $healthStatus")
 
+
+//  1 in 1..3
+//  (1..3).toList()
+//  1 in 3 downTo 1
+//  1 in 1 until 3
+//  3 in 1 until 3
+//  2 in 1..3
+//  2 !in 1..3
+//  'x' in 'a'..'z'
+
 }
