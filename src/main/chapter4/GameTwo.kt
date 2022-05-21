f85e4f1a17ceb92ab109902ebcdb6373e4bbbac0
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-05-20 21:45:42 -0500

    add new chapter from chapter 3

---
+++ src/main/chapter4/GameTwo.kt
@@ -0,0 +1,34 @@
+
+// Chapter 4
+
+fun main(args: Array<String>) {
+
+  val name = "Madrigal"
+  var healthPoints = 89
+  val isBlessed = true
+  val isInmortal = false
+
+  //Aura
+  val auraVisible = isBlessed && healthPoints > 50 || isInmortal
+  val auraColor = if (auraVisible) "GREEN" else "NONE"
+
+  println(auraColor)
+
+  val healthStatus = when (healthPoints) {
+    100 -> "is in a excelente condition !"
+    in 90..99 -> "has a few scratches."
+    in 75..89 -> if (isBlessed)
+      "has some  minor wounds but is healing quite quickly!"
+    else
+      "has some minor wounds."
+    in 15..74 -> "looks pretty hurt."
+    else -> "is an awful condition!"
+  }
+
+  // Player Status
+  println(
+    "(Aura: $auraColor)" +
+      "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+  )
+  
+}
