b5175cf0e0444b9813e7d1bbc97aa5c96c210e6f
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-06-09 19:05:38 -0500

    The anonymous function defines a variable and returns a greeting message string:

--- src/main/chapter5/SimVillage.kt
+++ src/main/chapter5/SimVillage.kt
@@ -2,5 +2,6 @@ fun main() {
   println({
     val currentYear = 2018
     "Welcome to SimVillage, Mayor! (copyright $currentYear)"
-  }())
+  }()  //The anonymous function defines a variable and returns a greeting message string:
+  )
 }
