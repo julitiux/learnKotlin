e1554213c4e58fe0d994ea90589c13d8d89416f2
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-06-16 14:50:33 -0500

    oder and ident code

--- src/main/chapter5/SimVillage.kt
+++ src/main/chapter5/SimVillage.kt
@@ -1,7 +1,8 @@
 fun main() {
-  println({
-    val currentYear = 2018
-    "Welcome to SimVillage, Mayor! (copyright $currentYear)"
-  }()  //The anonymous function defines a variable and returns a greeting message string:
+  println(
+    {
+      val currentYear = 2018
+      "Welcome to SimVillage, Mayor! (copyright $currentYear)"
+    }()  //The anonymous function defines a variable and returns a greeting message string:
   )
 }
