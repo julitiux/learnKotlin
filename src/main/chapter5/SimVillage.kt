b2d0e43bdb5f4e9913218d5fef59b2d73602f3ce
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-06-19 10:58:57 -0500

    Adding the runSimulation function, implement the fun

--- src/main/chapter5/SimVillage.kt
+++ src/main/chapter5/SimVillage.kt
@@ -9,3 +9,8 @@ fun main() {
   println(greetingFunction("Julito", 2))
 
 }
+
+fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+  val numBuildings = (1..3).shuffled().last() //Randomly selects 1,2, or 3
+  println(greetingFunction(playerName, numBuildings))
+}
