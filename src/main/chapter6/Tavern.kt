26cc09017925af9ed8762c86f253f28f2a4f74ac
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-09-24 11:59:30 -0500

    Using the null coalescing operator

--- src/main/chapter6/Tavern.kt
+++ src/main/chapter6/Tavern.kt
@@ -1,10 +1,14 @@
 fun main() {
   var beverage = readLine()
 //  beverage = null
+
   if (beverage != null) {
     beverage = beverage.capitalize()
   } else {
     println("I can't do that without crashing - beverage was null!")
   }
-  println(beverage)
+
+  val beverageServed: String = beverage ?: "Buttered Ale"
+  println(beverageServed)
+
 }
