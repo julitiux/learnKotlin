5289ff8379a7d68cc922f863adca9e017afd46d1
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-09-29 18:39:23 -0500

    Extracting the tavern master’s name

---
+++ src/main/chapter7/OtherTavern.kt
@@ -0,0 +1,12 @@
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
+fun main(args: Array<String>) {
+  placeOrder()
+}
+
+private fun placeOrder() {
+  val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+  val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+  println("Madrigal speaks with $tavernMaster about their order.")
+}
