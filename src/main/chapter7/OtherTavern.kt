d81c5fcbfbf9f3d8f1f0e8f53b3bd7a78be6b9d3
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-09-30 10:15:46 -0500

    Passing tavern data to placeOrder

--- src/main/chapter7/OtherTavern.kt
+++ src/main/chapter7/OtherTavern.kt
@@ -1,11 +1,10 @@
-
 const val TAVERN_NAME = "Taernyl's Folly"
 
 fun main(args: Array<String>) {
-  placeOrder()
+  placeOrder("shabdy, Dragon's Breath, 5.91")
 }
 
-private fun placeOrder() {
+private fun placeOrder(manuData: String) {
   val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
   val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
   println("Madrigal speaks with $tavernMaster about their order.")
