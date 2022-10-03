a50ec9fdecc8f2484ffa5ebba63891c5d44534be
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-10-03 14:05:59 -0500

    copy the same implemtn of TwoTaver.tk

--- src/main/chapter8/ThreeTavern.kt
+++ src/main/chapter8/ThreeTavern.kt
@@ -1,3 +1,36 @@
-fun main() {
-  
+const val TAVERN_NAME_3 = "Taernyl's Folly"
+
+fun main(args: Array<String>) {
+//  placeOrder("shabdy, Dragon's Breath, 5.91")
+  placeOrder("elixir, Shirley's Template, 4.12")
 }
+
+private fun placeOrder(menuData: String) {
+  val indexOfApostrophe = TAVERN_NAME_3.indexOf('\'')
+  val tavernMaster = TAVERN_NAME_3.substring(0 until indexOfApostrophe)
+  println("Madrigal speaks with $tavernMaster about their order.")
+
+  val (type, name, price) = menuData.split(',')
+  val message = "Madrigal buys a $name ($type) for $price"
+  println(message)
+
+  val phrase = if (name == "Dragon's Breath") {
+    "Madrigal exclaims: ${toDragonSpeak("Ah, delicius $name!")}"
+  } else {
+    "Madrigal says: thanks for the $name."
+  }
+  println(phrase)
+
+}
+
+private fun toDragonSpeak(phrase: String) =
+  phrase.replace(Regex("[aeiou]]")) {
+    when (it.value) {
+      "a" -> "4"
+      "e" -> "3"
+      "i" -> "1"
+      "o" -> "0"
+      "u" -> "|_|"
+      else -> it.value
+    }
+  }
