ca004370bcffccccd447d76b6aa9ce8130d28a93
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-10-04 12:50:46 -0500

    Here is an example of configuring a file instance without apply

--- src/main/chapter9/Chapter9.kt
+++ src/main/chapter9/Chapter9.kt
@@ -1,3 +1,11 @@
+import java.io.File
+
 fun main() {
-  
+
+//  Apply
+  val menuFile = File("menu-file.txt")
+  menuFile.setReadable(true)
+  menuFile.setWritable(true)
+  menuFile.setExecutable(false)
+
 }
