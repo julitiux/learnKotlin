00a375ea9073320ce644f8e05d7ba36cf8d03063
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-10-04 12:52:23 -0500

    Using apply, the same configuration can be achieved with less repetition:

--- src/main/chapter9/Chapter9.kt
+++ src/main/chapter9/Chapter9.kt
@@ -3,9 +3,18 @@ import java.io.File
 fun main() {
 
 //  Apply
-  val menuFile = File("menu-file.txt")
-  menuFile.setReadable(true)
-  menuFile.setWritable(true)
-  menuFile.setExecutable(false)
+//  Here is an example of configuring a file instance without apply
+//  val menuFile = File("menu-file.txt")
+//  menuFile.setReadable(true)
+//  menuFile.setWritable(true)
+//  menuFile.setExecutable(false)
+
+//  Using apply, the same configuration can be achieved with less repetition:
+  val menuFile = File("menu-file.txt").apply {
+    setReadable(true)
+    setWritable(true)
+    setExecutable(false)
+  }
+
 
 }
