798be92712e444c19ede0b9982823ea75b009d6c
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-10-14 17:43:25 -0500

    Defining a custom setter

--- src/main/chapter12/Player.kt
+++ src/main/chapter12/Player.kt
@@ -2,6 +2,9 @@ class Player {
 
   val name = "madrigal"
     get() = field.capitalize()
+    set(value) {
+      field = value.trim()
+    }
 
   fun castFireball(numFireballs: Int = 2) =
     println("A glass of Fireball springs into existence. (x$numFireballs)")
