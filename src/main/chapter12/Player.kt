e1898552d416ab0da33cf9dd28737828f60432da
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-10-17 17:15:35 -0500

    Adding class functions to Player

--- src/main/chapter12/Player.kt
+++ src/main/chapter12/Player.kt
@@ -10,6 +10,24 @@ class Player {
   val isBlessed = true
   private val isInmortal = false
 
+  private fun auraColor(isBlessed: Boolean, healthPoints: Int, isInmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isInmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+  }
+
+  private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
+      100 -> "is in a excelente condition !"
+      in 90..99 -> "has a few scratches."
+      in 75..89 -> if (isBlessed)
+        "has some  minor wounds but is healing quite quickly!"
+      else
+        "has some minor wounds."
+      in 15..74 -> "looks pretty hurt."
+      else -> "is an awful condition!"
+    }
+
   fun castFireball(numFireballs: Int = 2) =
     println("A glass of Fireball springs into existence. (x$numFireballs)")
 }
