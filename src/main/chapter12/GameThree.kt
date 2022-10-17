f566f180eea7c9794c2a2d6e82a9c54c619f0c0f
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-10-17 17:15:01 -0500

    Removing functions from main

--- src/main/chapter12/GameThree.kt
+++ src/main/chapter12/GameThree.kt
@@ -35,24 +35,6 @@ private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, healthStatu
   println("$name $healthStatus")
 }
 
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isInmortal: Boolean): String {
-  val auraVisible = isBlessed && healthPoints > 50 || isInmortal
-  val auraColor = if (auraVisible) "GREEN" else "NONE"
-  return auraColor
-}
-
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-  when (healthPoints) {
-    100 -> "is in a excelente condition !"
-    in 90..99 -> "has a few scratches."
-    in 75..89 -> if (isBlessed)
-      "has some  minor wounds but is healing quite quickly!"
-    else
-      "has some minor wounds."
-    in 15..74 -> "looks pretty hurt."
-    else -> "is an awful condition!"
-  }
-
 private fun shoudlBeAString(): String{
   TODO()
   println("unreachable")
