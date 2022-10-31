9ef0bad08f3d0ca02ce363f3cd399dd935ae3e12
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-10-31 07:18:26 -0600

    add a new line

--- src/main/chapter14/com/bignerdranch/nyethack/chapter14/Room.kt
+++ src/main/chapter14/com/bignerdranch/nyethack/chapter14/Room.kt
@@ -3,7 +3,7 @@ package com.bignerdranch.nyethack.chapter14
 open class Room(val name: String) {
   protected open val dangerLevel = 5
 
-  fun description() = "Room: $name" +
+  fun description() = "Room: $name\n" +
     "Danger level: $dangerLevel"
 
   open fun load() = "Nothing much to see here..."
