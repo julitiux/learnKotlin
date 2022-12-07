720f9e1c4e92d70004f07333359eb92bcb672713
Julio Ramirez <rrodriguez.julio@gmail.com>
2022-12-07 12:59:39 -0600

    Printing status in the game loop

--- src/main/chapter14/com/bignerdranch/nyethack/chapter14/Game.kt
+++ src/main/chapter14/com/bignerdranch/nyethack/chapter14/Game.kt
@@ -1,25 +1,28 @@
 package com.bignerdranch.nyethack.chapter14
 
 fun main(args: Array<String>) {
-  
-  println(currentRoom.description())
-  println(currentRoom.load())
 
-  // Aura
-  val auraColor = player.auraColor()
-  // Player Status
-  printPlayerStatus(player)
-  // Aura
-  player.auraColor()
+  Game.play()
+
 }
 
 object Game {
-
   private val player = Player("Julito")
   private var currentRoom: Room = TownSquare()
 
   init {
     println("Welcome, adventurer")
+  }
+
+  fun play() {
+    while (true) {
+      println(currentRoom.description())
+      println(currentRoom.load())
+
+      //Player status
+      printPlayerStatus(player)
+    }
+
   }
 
   private fun printPlayerStatus(player: Player) {
