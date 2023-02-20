8f047ce83b11f8acbcd8131406b50023cfbaed0c
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-02-20 01:25:28 -0600

    starting chapter 15

---
+++ src/main/chapter15/com/bignerdranch/nyethack/chapter14/Game.kt
@@ -0,0 +1,43 @@
+package com.bignerdranch.nyethack.chapter14
+
+fun main(args: Array<String>) {
+
+  Game.play()
+
+}
+
+object Game {
+  private val player = Player("Julito")
+  private var currentRoom: Room = TownSquare()
+
+  init {
+    println("Welcome, adventurer")
+  }
+
+  fun play() {
+    while (true) {
+      println(currentRoom.description())
+      println(currentRoom.load())
+
+      //Player status
+      printPlayerStatus(player)
+      print("> Enter your command: ")
+      println("Last command: ${readLine()}")
+    }
+
+  }
+
+  private fun printPlayerStatus(player: Player) {
+    println(
+      "(Aura: ${player.auraColor()})" +
+        "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
+    )
+    println("${player.name} ${player.formatHealthStatus()}")
+  }
+
+}
+
+private fun shoudlBeAString(): String {
+  TODO()
+  println("unreachable")
+}
