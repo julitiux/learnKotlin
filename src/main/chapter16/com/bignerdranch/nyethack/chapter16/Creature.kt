83f9d5d6ebb845af24544ded517c00d3f9b67bdb
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-03-21 22:17:04 -0600

    Defining a default getter implementation (Creature.kt)

--- src/main/chapter16/com/bignerdranch/nyethack/chapter16/Creature.kt
+++ src/main/chapter16/com/bignerdranch/nyethack/chapter16/Creature.kt
@@ -1,10 +1,15 @@
 package com.bignerdranch.nyethack.chapter16
 
+import kotlin.random.Random
+
 interface Fightable {
   var healtPoints: Int
   val diceCount: Int
   val diceSides: Int
   val damageRoll: Int
+    get() = (0 until diceCount).map {
+      Random().nextInt(diceCount + 1)
+    }.sum()
 
   fun attack(opponent: Fightable): Int
 }
