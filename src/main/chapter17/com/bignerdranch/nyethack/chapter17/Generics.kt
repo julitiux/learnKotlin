b19ce257aef46a9f9cb36c17225c5237fe01915c
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-03-27 01:16:01 -0600

    Defining loot boxes

--- src/main/chapter17/com/bignerdranch/nyethack/chapter17/Generics.kt
+++ src/main/chapter17/com/bignerdranch/nyethack/chapter17/Generics.kt
@@ -1,5 +1,13 @@
 package com.bignerdranch.nyethack.chapter17
 
-class LootBox<T>(item: T){
+class LootBox<T>(item: T) {
   private var loot: T = item
+}
+
+class Fedora(val name: String, val value: Int)
+class Coin(val value: Int)
+
+fun main(args: Array<String>){
+  var lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
+  var lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
 }
