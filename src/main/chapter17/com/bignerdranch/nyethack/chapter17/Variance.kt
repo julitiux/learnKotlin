3477946cc3dbb4168ffb06035b8c5da15c86e493
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-03-28 13:19:51 -0600

    Reversing the Assigment

--- src/main/chapter17/com/bignerdranch/nyethack/chapter17/Variance.kt
+++ src/main/chapter17/com/bignerdranch/nyethack/chapter17/Variance.kt
@@ -1,12 +1,10 @@
 package com.bignerdranch.nyethack.chapter17
 
-class Barrel<out T>(val item: T)
+class Barrel<in T>(item: T)
 
 fun main(args: Array<String>) {
   var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
   var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
 
-  lootBarrel = fedoraBarrel
-
-  val myFedora : Fedora = lootBarrel.item
+  fedoraBarrel = lootBarrel
 }
