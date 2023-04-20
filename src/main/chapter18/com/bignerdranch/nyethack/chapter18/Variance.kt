b1b4bdc95f43f19436ca9d9218d63594215b98b4
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-04-20 16:50:27 -0600

    add chapter18 to Extensions

---
+++ src/main/chapter18/com/bignerdranch/nyethack/chapter18/Variance.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.nyethack.chapter18
+
+class Barrel<in T>(item: T)
+
+fun main(args: Array<String>) {
+  var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
+  var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+
+  fedoraBarrel = lootBarrel
+}
