4f9a68a12f711feec82124b96fda25757c6e3190
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-04-20 17:19:58 -0600

    Calling the new extension on a String receiver instance (Extension.tk)

--- src/main/chapter18/com/bignerdranch/nyethack/chapter18/Extensions.kt
+++ src/main/chapter18/com/bignerdranch/nyethack/chapter18/Extensions.kt
@@ -1,3 +1,7 @@
 package com.bignerdranch.nyethack.chapter18
 
-fun String.addEnthusiam(amount: Int = 1) = this + "!" .repeat(amount)
+fun String.addEnthusiam(amount: Int = 1) = this + "!".repeat(amount)
+
+fun main(args: Array<String>) {
+  println("Mandrigal has left the building".addEnthusiam())
+}
