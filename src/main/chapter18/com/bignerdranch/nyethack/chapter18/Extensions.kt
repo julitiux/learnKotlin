f8ab4a447f2d68cda4b386ca5a6c42e45a3de833
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-04-21 02:36:03 -0600

    making easyPrint chainable

--- src/main/chapter18/com/bignerdranch/nyethack/chapter18/Extensions.kt
+++ src/main/chapter18/com/bignerdranch/nyethack/chapter18/Extensions.kt
@@ -2,7 +2,10 @@ package com.bignerdranch.nyethack.chapter18
 
 fun String.addEnthusiam(amount: Int = 1) = this + "!".repeat(amount)
 
-fun Any.easyPrint() = println(this)
+fun Any.easyPrint(): Any {
+  println(this)
+  return this
+}
 
 fun main(args: Array<String>) {
   "Mandrigal has left the building".addEnthusiam().easyPrint()
