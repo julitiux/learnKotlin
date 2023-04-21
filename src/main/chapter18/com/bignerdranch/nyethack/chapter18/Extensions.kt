7a7b59bc0d851e23af8f81ab22f963cf281708e6
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-04-20 19:52:01 -0600

    extending Any (Extensions.kt)

--- src/main/chapter18/com/bignerdranch/nyethack/chapter18/Extensions.kt
+++ src/main/chapter18/com/bignerdranch/nyethack/chapter18/Extensions.kt
@@ -2,6 +2,8 @@ package com.bignerdranch.nyethack.chapter18
 
 fun String.addEnthusiam(amount: Int = 1) = this + "!".repeat(amount)
 
+fun Any.easyPrint() = println(this)
+
 fun main(args: Array<String>) {
-  println("Mandrigal has left the building".addEnthusiam())
+  "Mandrigal has left the building".addEnthusiam().easyPrint()
 }
