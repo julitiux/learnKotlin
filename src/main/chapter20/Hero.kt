72ce2faac935c708b1fa3b2e429cfb188dd2aba3
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-04-27 14:51:18 -0600

    Printing the friendship level (Hero.kt)

--- src/main/chapter20/Hero.kt
+++ src/main/chapter20/Hero.kt
@@ -1,4 +1,7 @@
 fun main() {
   val adversary = Jhava()
   println(adversary.utterGreenting())
+
+  val friendshipLevel = adversary.determineFriendshipLevel()
+  println(friendshipLevel.toLowerCase())
 }
