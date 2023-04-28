c44775af73ec9444f790c36061dc52c709183413
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-04-28 01:58:34 -0600

    Declaring a field in java (Jhave.java)

--- src/main/chapter20/Jhava.java
+++ src/main/chapter20/Jhava.java
@@ -3,15 +3,19 @@ import com.sun.istack.internal.Nullable;
 
 public class Jhava {
 
-  public int hitPoints = 52489112;
+  private int hitPoints = 52489112;
 
   @NotNull
   public String utterGreenting() {
     return "BLARGH";
   }
-  
+
   @Nullable
-  public String determineFriendshipLevel(){
+  public String determineFriendshipLevel() {
     return null;
+  }
+
+  public int hetHitPoints() {
+    return hitPoints;
   }
 }
